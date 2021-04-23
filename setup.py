60d80ea137b7e7df5e483ebd668509f6a882655b
Brad Cowie <brad@wand.net.nz>
2021-04-23 15:42:54 +1200

    DEBUG

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ def install_configs():
             raise
 
 setup(
-    name='faucet',
+    name='gizmoguy-faucet',
     setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
     python_requires='>=3.5',
     pbr=True
