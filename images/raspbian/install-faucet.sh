4de4a1190e0e87ad96167406c446f50e9f9e5de1
Brad Cowie <brad@wand.net.nz>
2021-04-23 15:42:54 +1200

    DEBUG

--- images/raspbian/install-faucet.sh
+++ images/raspbian/install-faucet.sh
@@ -5,8 +5,8 @@ ID="$(grep -Po '(?<=^ID=).*' "${ROOTFS_DIR}/etc/os-release" | awk '{print tolowe
 CODE="$(grep -Po -m 1 '(?<=\()[^\)]+' "${ROOTFS_DIR}/etc/os-release" | awk '{print tolower($0)}')"
 on_chroot << EOF
 apt-get install -y curl apt-transport-https lsb-release tcpdump
-echo "deb https://packagecloud.io/faucetsdn/faucet/${ID}/ ${CODE} main" | tee /etc/apt/sources.list.d/faucet.list
-curl -L https://packagecloud.io/faucetsdn/faucet/gpgkey | apt-key add -
+echo "deb https://packagecloud.io/faucetsdn/faucet-test/${ID}/ ${CODE} main" | tee /etc/apt/sources.list.d/faucet.list
+curl -L https://packagecloud.io/faucetsdn/faucet-test/gpgkey | apt-key add -
 apt-get update
 apt-get install -y --fix-missing faucet-all-in-one
 apt-get clean
